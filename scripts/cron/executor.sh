1d691c17d260a98cfbe5f12040e2f32ee57c4d8f
NicolasCBV <nicolascbv700@gmail.com>
2025-04-16 01:03:52 -0300

    fix: daemon early dispatch

--- scripts/cron/executor.sh
+++ scripts/cron/executor.sh
@@ -2,37 +2,55 @@
 
 printf '%b' "Collecting Open Weather data through user IP...\n"
 
-cache_dir="/tmp/weather.json"
-if [ -f $cache_dir ]; then
-  rm $cache_dir
-fi
+max_attempts=5
+attempt=1
 
-payload=$(curl -w ":%{http_code}" -s ipinfo.io)
-status="${payload##*:}"
-body="${payload%:*}"
-if [ $((status)) -gt 399 ] || [ $((status)) -eq 0 ]; then
-  msg="Could not read geolocation using user IP!"
-  printf '%b' "${msg}\n"
-  echo "${msg}" | systemd-cat -t weather -p "err"
-  exit 1
-fi
+while true; do
+  cache_dir="/tmp/weather.json"
+  if [ -f $cache_dir ]; then
+    rm $cache_dir
+  fi
 
-geolocation=($(curl -s ipinfo.io | jq -r '.loc' | tr ',' ' '))
+  payload=$(curl -w ":%{http_code}" -s ipinfo.io)
+  status="${payload##*:}"
+  body="${payload%:*}"
+  if [ $((status)) -gt 399 ] || [ $((status)) -eq 0 ]; then
+    if [ $attempt -eq $max_attempts ]; then
+      msg="Could not read geolocation using user IP! Status code: ${status}"
+      printf '%b' "${msg}\n"
+      echo "${msg}" | systemd-cat -t weather -p "err"
+      exit 1
+    else
+      sleep 8
+      attempt=$((attempt+1))
+      continue
+    fi
+  fi
 
-lat="${geolocation[0]}"
-lon="${geolocation[1]}"
+  geolocation=($(curl -s ipinfo.io | jq -r '.loc' | tr ',' ' '))
 
-cred=$(cat /etc/minimalist_conquer/cred.json | jq -r '.api_key')
-status=$(curl -w "%{http_code}" -s "https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${cred}" -o $cache_dir)
+  lat="${geolocation[0]}"
+  lon="${geolocation[1]}"
 
-if [ $((status)) -gt 399 ] || [ $((status)) -eq 0 ]; then
-  msg="Could not read Open Weather API!"
-  printf '%b' "${msg}\n"
-  echo "${msg}" | systemd-cat -t weather -p "err"
-  exit 1
-fi
+  cred=$(cat /etc/minimalist_conquer/cred.json | jq -r '.api_key')
+  status=$(curl -w "%{http_code}" -s "https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${cred}" -o $cache_dir)
 
-touch $cache_dir
-echo "${res}" >> $cache_dir
+  if [ $((status)) -gt 399 ] || [ $((status)) -eq 0 ]; then
+    if [ $attempt -eq $max_attempts ]; then
+      msg="Could not read Open Weather API! Status code: ${status}"
+      printf '%b' "${msg}\n"
+      echo "${msg}" | systemd-cat -t weather -p "err"
+      exit 1
+    else
+      sleep 8
+      attempt=$((attempt+1))
+      continue
+    fi
+  fi
+
+  touch $cache_dir
+  echo "${res}" >> $cache_dir
+  break
+done
 
 printf '%b' "Successfully cached Open Weather data! Exiting.\n"
