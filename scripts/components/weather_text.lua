9c9c3fe2fd77f30e096bcfc8e5f99440d96f3c41
Nícolas Basilio <nicolascbv700@gmail.com>
2025-07-04 00:55:06 -0300

    fix: waether description style

--- scripts/components/weather_text.lua
+++ scripts/components/weather_text.lua
@@ -40,15 +40,15 @@ function Draw_weather_text(cr, weather_data)
     local weather_text = build_text(weather_data)
 
     cairo_set_source_rgba(cr, 0.63, 0.63, 0.70, 0.5)
-    cairo_rectangle(cr, 43, 210, 2, 38)
+    cairo_rectangle(cr, 45, 210, 2, 34)
     cairo_fill(cr)
 
     cairo_select_font_face(cr, "Poppins Normal", CAIRO_FONT_SLANT_NORMAL)
     cairo_set_source_rgb(cr, 0.13, 0.13, 0.18, 1)
-    cairo_set_font_size(cr, 12)
+    cairo_set_font_size(cr, 11)
 
     local x = 55
-    local y = 225
+    local y = 223
     cairo_move_to(cr, x, y)
-    Write_text(cr, weather_text, x, y, 320, 13)
+    Write_text(cr, weather_text, x, y, 340, 13)
 end
